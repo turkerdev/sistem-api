b516164660eab0f4f951d10d0a32dbc07d7e20f7
unknown <oqzcmcuxr@mozmail.com>
2022-03-20 14:52:16 +0300

    feat: add logger

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log("sistem");
+import { logger } from "./utils/logger.js";
+
+logger.info("API process started");
+
+process.on("exit", (code) => {
+  logger.info({ code }, `API process exiting`);
+});
+
+process.on("uncaughtException", (err) => {
+  logger.fatal({ err }, "Uncaught Exception");
+});
