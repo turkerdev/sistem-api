d2eb6a173e1259bb54a728694e576e366881483a
unknown <oqzcmcuxr@mozmail.com>
2022-03-23 09:09:30 +0300

    feat: lint-staged prisma file

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -5,4 +5,6 @@ export default {
     `"tsc -p tsconfig.json --pretty --noEmit"`,
   // Pretty staged files
   "*": (files) => `prettier --ignore-unknown --write ${files.join(" ")}`,
+  // Fix staged prisma files
+  "*.prisma": (files) => files.map((file) => `prisma format --schema ${file}`),
 };
